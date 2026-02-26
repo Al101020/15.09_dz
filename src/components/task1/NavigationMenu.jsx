759873603d4c3d5e82c577df516d95b55739024b
Al101020 <alex.vl.m@yandex.ru>
2026-02-26 17:24:04 +1000

    commit-0.1

---
+++ src/components/task1/NavigationMenu.jsx
@@ -0,0 +1,10 @@
+import './NavigationMenu.css';
+import { useEffect, useState } from 'react';
+
+function NavigationMenu() {
+  return (
+    <div className='task1'></div>
+  )
+};
+
+export default NavigationMenu;
---
+++ src/components/task2/Crud.jsx
@@ -0,0 +1,10 @@
+import './Crud.css';
+import { useEffect, useState } from 'react';
+
+function Crud() {
+  return (
+    <div className='task2'></div>
+  )
+};
+
+export default Crud;
